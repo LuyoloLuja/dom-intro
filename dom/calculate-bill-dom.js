d751746f74164a5ca43f72b7fb41040dfc4970e5
LuyoloLuja <luyolom19@gmail.com>
2020-06-04 17:07:41 +0200

    linked calculate bill factory function to the dom

--- dom/calculate-bill-dom.js
+++ dom/calculate-bill-dom.js
@@ -1,53 +1,30 @@
 const calculateBtn = document.querySelector(".calculateBtn");
-
 const billTotalElement = document.querySelector(".billTotal");
-
 const billStringField = document.querySelector(".billString");
-
 const billTotalSpanElement = document.querySelector(".total");
 
-// total calculation for the bill
+var calculateBillInstance = CalculateBill();
+
 function totalPhoneBill(billString){
-
     var billItems = billString.split(",");
-
-    var billTotal = 0;
-
-    for(var i = 0; i < billItems.length; i++){
-        var billItem = billItems[i].trim();
-        if(billItem === "call"){
-            billTotal += 2.75;
-        }
-        else if(billItem === "sms"){
-            billTotal += 0.75;
-        }
-    }
-    
-    // round to two decimals
-    var roundedBillTotal = billTotal.toFixed(2);
-    return roundedBillTotal;
+    calculateBillInstance.billType(billItems);
+    calculateBillInstance.returnTotal().toFixed(2);
 }
 
 // when the button is clicked
 function calculateBtnClicked(){
     var billString = billStringField.value;
-    const roundedBillTotal = totalPhoneBill(billString);
-    billTotalElement.innerHTML = roundedBillTotal;
-    styleTotalColor(roundedBillTotal);
+    calculateBillInstance.billType(billString);
+    billTotalElement.innerHTML = calculateBillInstance.returnTotal().toFixed(2);
+    styleTotalColor(calculateBillInstance.returnTotal());
 }
 calculateBtn.addEventListener("click", calculateBtnClicked);
 
 // color for the totals
-function   styleTotalColor(roundedBillTotal){
-    const currentTotal = Number(roundedBillTotal);
-    billTotalSpanElement.classList.remove("critical");
-    billTotalSpanElement.classList.remove("warning");
+function   styleTotalColor(){
+    billTotalElement.classList.remove("critical");
+    billTotalElement.classList.remove("warning");
 
-    if(currentTotal >= 30){
-        //make the total red
-        billTotalSpanElement.classList.add("critical");
-    }else if(currentTotal >= 20 && currentTotal < 30){
-        // make the total orange
-        billTotalSpanElement.classList.add("warning");
-    }
+    billTotalElement.classList.add(calculateBillInstance.color());
+    
 }
--- factory-functions/calculate.js
+++ factory-functions/calculate.js
@@ -1,18 +1,45 @@
-function calculateBill(billElement) {
-    var billItems = billElement;
-    var billType = billItems.split(',');
-    var call = 0;
-    var sms = 0;
+function CalculateBill(){
+  var call = 0;
+  var sms = 0;
+  var overallTotal = 0;
 
-    for (var i = 0; i < billType.length; i++) {
-      var bill = billType[i].trim();
-      if (bill === 'call') {
-        call += 2.75;
-      }
-      else if (bill === 'sms') {
-        sms += 0.75;
-      }
+  var warningValue = 0;
+  var criticalValue = 0;
+
+  function returnCall(){
+    return call;
+  }
+
+  function returnSms(){
+    return sms;
+  }
+
+  function returnTotal(){
+    overallTotal = call + sms;
+    return overallTotal;
+  }
+
+  function billType(bill){
+    if(bill === "call"){
+      call += 2.75;
+    } else if(bill === "sms"){
+      sms += 0.75
     }
-    var total = call + sms;
-    return total.toFixed(2);
+  }
+
+  function color(){
+    if(returnTotal() >= warningValue && returnTotal() < criticalValue){
+      return "warning";
+    } else if(returnTotal() >= criticalValue){
+      return "critical";
+    }
+  }
+
+  return {
+    returnCall,
+    returnSms,
+    returnTotal,
+    billType,
+    color
+  }
 }
