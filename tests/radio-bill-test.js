6de882be96dacf6c8996a26eef9d060d5646ec35
LuyoloLuja <luyolom19@gmail.com>
2020-06-09 13:36:33 +0200

    fixed failing tests

--- tests/radio-bill-test.js
+++ tests/radio-bill-test.js
@@ -1,26 +1,101 @@
 describe('The radio-bill function', function(){
     it('Should be able to add call at 2.75', function(){
         var itemType = RadioBill();
-        itemType.getCall(2.75);
-        assert.equal(2.75, itemType.getCall())
-    })
-    it('Should be able to add sms 0.75', function(){
-        var itemTypeTwo = RadioBill();
-        itemTypeTwo.getSms(0.75);
-        assert.equal(0.75, itemTypeTwo.getSms())
-    })
-    it('Should be able to add both call and sms', function(){
-        var billRadio = RadioBill();
 
-        billRadio.getCall(2.75);
-        billRadio.getSms(0.75);
-
-        assert.equal(2.75, billRadio.getCall());
-        assert.equal(0.75, billRadio.getSms());
+        itemType.radioButtons("call");
+        assert.equal(itemType.getCall(), 2.75);
     })
-    it('Should be able to overall total', function(){
-        var radioBillItem = RadioBill();
+    it('Should be able to send sms at 0.75', function(){
+        var itemType = RadioBill();
 
-        assert.equal(3.50, radioBillItem.getTotal());
+        itemType.radioButtons("sms");
+        assert.equal(itemType.getSms(), 0.75)
+    })
+    it('Should be able to return total', function(){
+        var itemType = RadioBill();
+
+        itemType.radioButtons("call");
+        itemType.radioButtons("sms");
+        assert.equal(itemType.getTotal(), 3.50);
+    })
+    it('Should be able to add more than one call', function(){
+        var itemType = RadioBill();
+
+        itemType.radioButtons("call");
+        itemType.radioButtons("call");
+        itemType.radioButtons("call");
+        assert.equal(itemType.getCall(), 8.25);
+    })
+    it('Should be able to send more than one sms', function(){
+        var itemType = RadioBill();
+
+        itemType.radioButtons("sms");
+        itemType.radioButtons("sms");
+        itemType.radioButtons("sms");
+        assert.equal(itemType.getSms(), 2.25)
+    })
+    it('Should return a class name of warning if total reached 30', function(){
+        var itemType = RadioBill();
+
+        itemType.radioButtons("sms");
+        itemType.radioButtons("sms");
+        itemType.radioButtons("sms");
+        itemType.radioButtons("sms");
+        itemType.radioButtons("sms");
+        itemType.radioButtons("sms");
+        itemType.radioButtons("sms");
+        itemType.radioButtons("sms");
+        itemType.radioButtons("sms");
+        itemType.radioButtons("call");
+        itemType.radioButtons("call");
+        itemType.radioButtons("call");
+        itemType.radioButtons("call");
+        itemType.radioButtons("call");
+        itemType.radioButtons("call");
+        itemType.radioButtons("call");
+        itemType.radioButtons("call");
+        itemType.radioButtons("call");
+        assert.equal(itemType.totalClassName(), "warning");
+    })
+    it('Should return a class name of critical if total reached 50', function(){
+        var itemType = RadioBill();
+
+        itemType.radioButtons("sms");
+        itemType.radioButtons("sms");
+        itemType.radioButtons("sms");
+        itemType.radioButtons("sms");
+        itemType.radioButtons("sms");
+        itemType.radioButtons("sms");
+        itemType.radioButtons("sms");
+        itemType.radioButtons("sms");
+        itemType.radioButtons("sms");
+        itemType.radioButtons("call");
+        itemType.radioButtons("call");
+        itemType.radioButtons("call");
+        itemType.radioButtons("call");
+        itemType.radioButtons("call");
+        itemType.radioButtons("call");
+        itemType.radioButtons("call");
+        itemType.radioButtons("call");
+        itemType.radioButtons("call");
+        itemType.radioButtons("sms");
+        itemType.radioButtons("sms");
+        itemType.radioButtons("sms");
+        itemType.radioButtons("sms");
+        itemType.radioButtons("sms");
+        itemType.radioButtons("sms");
+        itemType.radioButtons("sms");
+        itemType.radioButtons("sms");
+        itemType.radioButtons("sms");
+        itemType.radioButtons("call");
+        itemType.radioButtons("call");
+        itemType.radioButtons("call");
+        itemType.radioButtons("call");
+        itemType.radioButtons("call");
+        itemType.radioButtons("call");
+        itemType.radioButtons("call");
+        itemType.radioButtons("call");
+        itemType.radioButtons("call");
+        assert.equal(itemType.totalClassName(), "critical");
     })
 })
--- tests/text-bill-test.js
+++ tests/text-bill-test.js
@@ -1,26 +1,98 @@
-describe('The text-bill function', function(){
+describe("The text bill function", function(){
     it('Should be able to add call at 2.75', function(){
-        var billValue = textBill();
-        billValue.getCallCost(2.75);
-        assert.equal(2.75, billValue.getCallCost())
+        var billType = TextBill();
+        
+        billType.billEntered("call");
+        assert.equal(billType.callCostTotal(), 2.75);
     })
-    it('Should be able to add sms 0.75', function(){
-        var billValueTwo = textBill();
-        billValueTwo.getSmsCost(0.75);
-        assert.equal(0.75, billValueTwo.getSmsCost())
+    it('Should be able to send sms at 0.75', function(){
+        var billType = TextBill();
+        
+        billType.billEntered("sms");
+        assert.equal(billType.smsCostTotal(), 0.75);
     })
-    it('Should be able to add both call and sms', function(){
-        var billText = textBill();
+    it("Should be able to return total", function(){
+        var billType = TextBill();
 
-        billText.getCallCost(2.75);
-        billText.getSmsCost(0.75);
-
-        assert.equal(2.75, billText.getCallCost());
-        assert.equal(0.75, billText.getSmsCost());
+        billType.billEntered("call");
+        billType.billEntered("sms");
+        assert.equal(billType.costTotal(), 3.50)
     })
-    it('Should be able to overall total', function(){
-        var billItem = textBill();
+    it("Should be able to add more than one call", function(){
+        var billType = TextBill();
 
-        assert.equal(3.50, billItem.roundedTotalCost());
+        billType.billEntered("call");
+        billType.billEntered("call");
+        billType.billEntered("call");
+        assert.equal(billType.callCostTotal(), 8.25)
+    })
+    it("Should be able to add more than one sms", function(){
+        var billType = TextBill();
+
+        billType.billEntered("sms");
+        billType.billEntered("sms");
+        billType.billEntered("sms");
+        assert.equal(billType.smsCostTotal(), 2.25)
+    })
+    it("Should return a class name of warning if total reached 30", function(){
+        var billType  = TextBill();
+
+        billType.billEntered("call");
+        billType.billEntered("call");
+        billType.billEntered("call");
+        billType.billEntered("call");
+        billType.billEntered("call");
+        billType.billEntered("call");
+        billType.billEntered("sms");
+        billType.billEntered("sms");
+        billType.billEntered("sms");
+        billType.billEntered("sms");
+        billType.billEntered("sms");
+        billType.billEntered("sms");
+        billType.billEntered("call");
+        billType.billEntered("call");
+        billType.billEntered("call");
+        billType.billEntered("call");
+        billType.billEntered("call");
+        billType.billEntered("call");
+        billType.billEntered("call");
+        billType.billEntered("call");
+
+        assert.equal(billType.levelColor(), "warning");
+    })
+    it("Should return a class name of critical if total reached 50", function(){
+        var billType  = TextBill();
+
+        billType.billEntered("call");
+        billType.billEntered("call");
+        billType.billEntered("call");
+        billType.billEntered("call");
+        billType.billEntered("call");
+        billType.billEntered("call");
+        billType.billEntered("sms");
+        billType.billEntered("sms");
+        billType.billEntered("sms");
+        billType.billEntered("sms");
+        billType.billEntered("sms");
+        billType.billEntered("sms");
+        billType.billEntered("call");
+        billType.billEntered("call");
+        billType.billEntered("call");
+        billType.billEntered("call");
+        billType.billEntered("call");
+        billType.billEntered("call");
+        billType.billEntered("call");
+        billType.billEntered("call");
+        billType.billEntered("sms");
+        billType.billEntered("call");
+        billType.billEntered("call");
+        billType.billEntered("call");
+        billType.billEntered("call");
+        billType.billEntered("call");
+        billType.billEntered("call");
+        billType.billEntered("call");
+        billType.billEntered("call");
+
+        assert.equal(billType.levelColor(), "critical");
     })
 })
