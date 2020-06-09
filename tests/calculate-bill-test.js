00a190218049f25a4196c3dda363fd0ab26f07fd
LuyoloLuja <luyolom19@gmail.com>
2020-06-09 14:51:22 +0200

    fixed failing tests for calculate bill

--- tests/calculate-bill-test.js
+++ tests/calculate-bill-test.js
@@ -1,17 +1,79 @@
 describe('The calculateBill function' , function(){
     it('Should check if call cost is equal to 2.75' , function(){
-        assert.equal(calculateBill('call'), 2.75 )
+        var billItemType = CalculateBill();
+        billItemType.billType("call");
+
+        assert.equal(billItemType.returnCall(), 2.75 );
     });
     it('Should  check if sms cost is equal to 0.75' , function(){
-        assert.equal(calculateBill('sms'), 0.75 )
+        var billItemType = CalculateBill();
+        billItemType.billType("sms");
+
+        assert.equal(billItemType.returnSms(), 0.75 );
     });
     it('Should be able to add more than one call bill', function(){
-        assert.equal(calculateBill('call, call, call, call, call'), 13.75)
+        var billItemType = CalculateBill();
+        billItemType.billType("call");
+        billItemType.billType("call");
+        billItemType.billType("call");
+
+        assert.equal(billItemType.returnCall(), 8.25);
     })
     it('Should be able to add more that one sms bill' , function(){
-        assert.equal(calculateBill('sms, sms, sms, sms, sms'), 3.75 )
+        var billItemType = CalculateBill();
+        billItemType.billType("sms");
+        billItemType.billType("sms");
+        billItemType.billType("sms");
+
+        assert.equal(billItemType.returnSms(), 2.25 );
     });
-    it('Should be able to add both call and sms' , function(){
-        assert.equal(calculateBill('call, sms, call, sms, call, sms'),10.50 )
+    it('Should be able to add both call and sms and return total' , function(){
+        var billItemType = CalculateBill();
+        billItemType.billType("call");
+        billItemType.billType("sms");
+
+        assert.equal(billItemType.returnTotal(), 3.50);
     });
+    it('Should return a class name of warning if total reached 20', function(){
+        var billItemType = CalculateBill();
+
+        billItemType.billType("call");
+        billItemType.billType("call");
+        billItemType.billType("call");
+        billItemType.billType("sms");
+        billItemType.billType("sms");
+        billItemType.billType("sms");
+        billItemType.billType("call");
+        billItemType.billType("call");
+        billItemType.billType("call");
+        billItemType.billType("sms");
+        billItemType.billType("sms");
+        billItemType.billType("sms");
+
+        assert.equal(billItemType.color(), "warning");
+    })
+    it('Should return a class name of warning if total reached 30', function(){
+        var billItemType = CalculateBill();
+
+        billItemType.billType("call");
+        billItemType.billType("call");
+        billItemType.billType("call");
+        billItemType.billType("sms");
+        billItemType.billType("sms");
+        billItemType.billType("sms");
+        billItemType.billType("call");
+        billItemType.billType("call");
+        billItemType.billType("call");
+        billItemType.billType("sms");
+        billItemType.billType("sms");
+        billItemType.billType("sms");
+        billItemType.billType("call");
+        billItemType.billType("call");
+        billItemType.billType("call");
+        billItemType.billType("call");
+        billItemType.billType("call");
+        billItemType.billType("call");
+
+        assert.equal(billItemType.color(), "critical");
+    })
 });
