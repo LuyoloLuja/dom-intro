6491a6cfb7e09515d93a124030b5fa1c1fe89139
LuyoloLuja <luyolom19@gmail.com>
2020-06-10 14:36:26 +0200

    fixed calculate bill to not update unless bills are entered manually as string

--- dom/calculate-bill-dom.js
+++ dom/calculate-bill-dom.js
@@ -1,23 +1,18 @@
-const calculateBtn = document.querySelector(".calculateBtn");
-const billTotalElement = document.querySelector(".billTotal");
-const billStringField = document.querySelector(".billString");
-const billTotalSpanElement = document.querySelector(".total");
+const calculateBtn = document.querySelector(".calculateBtn"); 
 
-var calculateBillInstance = CalculateBill();
+function calculateBtnClicked(){    
+    const billTotalElement = document.querySelector(".billTotal");
+    const billStringField = document.querySelector(".billString");
+    const billTotalSpanElement = document.querySelector(".total");
 
-function calculateBtnClicked(){
+    var calculateBillInstance = CalculateBill();
     var billString = billStringField.value;
+
     calculateBillInstance.billType(billString);
     billTotalElement.innerHTML = calculateBillInstance.returnTotal().toFixed(2);
-    styleTotalColor(calculateBillInstance.returnTotal());
-}
-calculateBtn.addEventListener("click", calculateBtnClicked);
 
-// color for the totals
-function   styleTotalColor(){
     billTotalElement.classList.remove("critical");
     billTotalElement.classList.remove("warning");
-
-    billTotalElement.classList.add(calculateBillInstance.color());
-    
+    billTotalSpanElement.classList.add(calculateBillInstance.color());
 }
+calculateBtn.addEventListener("click", calculateBtnClicked);
